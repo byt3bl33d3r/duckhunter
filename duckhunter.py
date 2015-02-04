58733b1f102c780171f47fe01f104d38056b932c
Binky Bear <binkybear@nethunter.com>
2015-02-03 20:30:42 -0600

    Add user input for outfile location
    Also closed source and destination file.  Removed tmp.txt after script is done.
    
    Outputs location of saved file also.
    
    Signed-off-by: Binky Bear <binkybear@nethunter.com>

--- duckhunter.py
+++ duckhunter.py
@@ -12,13 +12,14 @@ from decimal import Decimal #for conversion milliseconds -> seconds
 parser = argparse.ArgumentParser(description='Converts USB rubber ducky scripts to a Nethunter format', epilog="Quack Quack")
 parser.add_argument('-l', type=str, dest='layout', choices=['us', 'fr', 'de', 'es','sv', 'it', 'uk', 'ru','dk','no','pt','be'], help='Keyboard layout')
 parser.add_argument('duckyscript', help='Ducky script to convert')
+parser.add_argument('hunterscript', help='Output script')
 
 args = parser.parse_args()
 
 # Input file is argument / output file is output.txt
 infile = open(args.duckyscript)
+dest = open(args.hunterscript, 'w')
 tmpfile = open("tmp.txt", "w")
-#args.layout = sys.argv[2]
 
 def duckyRules (source):
 
@@ -100,7 +101,6 @@ if __name__ == "__main__":
 		tmpfile.close()
 
 	src = open('tmp.txt', 'r')
-	dest = open('output.txt', 'w')
 	for line in src:
 
 		if line.startswith('sleep'):
@@ -136,4 +136,7 @@ if __name__ == "__main__":
 		else:
 			dest.write('%s%s%s\n' % (prefixinput, line.rstrip('\n').strip(), suffix))
 
-	print "File saved to output.txt"
+	src.close()
+	dest.close()
+	os.remove("tmp.txt")
+    print "File saved to locaiton: " + (args.hunterscript)
